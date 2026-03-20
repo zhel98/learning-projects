f2bf65416f51dc754a0a4a3cc798fa03eab6fffe
ZHEL <47091459+zhel98@users.noreply.github.com>
2026-03-20 16:01:59 +0600

    123

--- python/GeeksHW6/GeeksHW6.py
+++ python/GeeksHW6/GeeksHW6.py
@@ -1,34 +1,37 @@
+while True:
 
-password = input("Введите свой пароль: ")
+    password = input("Введите свой пароль ('q'-чтобы выйти): ")
+    if password == 'q':
+         break
 
-def is_pass_gucci(password) -> bool:
-       
-        score = 0
-        special_counter = 0
-
-        if len(password) < 6:
-            return False
-        else: score +=1
-        if any(character.islower() for character in password):
-             score +=1
-        if any(character.isupper() for character in password):
-            score +=1 
-        if any(character.isdigit() for character in password):
-             score +=1
+    def is_pass_gucci(password) -> bool:
         
-        for character in password:
-            if not character.isalnum():
-                special_counter += 1
-        if special_counter >= 2:
-                score +=1
-        if score > 4: 
-             return True
-        else: 
-             return False
+            score = 0
+            special_counter = 0
 
-if is_pass_gucci(password) == True:
-     print("My man ur pass is gucci")
-else:
-     print("Fool. Try again")
+            if len(password) < 6:
+                return False
+            else: score +=1
+            if any(character.islower() for character in password):
+                score +=1
+            if any(character.isupper() for character in password):
+                score +=1 
+            if any(character.isdigit() for character in password):
+                score +=1
+            
+            for character in password:
+                if not character.isalnum():
+                    special_counter += 1
+            if special_counter >= 2:
+                    score +=1
+            if score > 4: 
+                return True
+            else: 
+                return False
+
+    if is_pass_gucci(password) == True:
+        print("My man ur pass is gucci")
+    else:
+        print("Fool. Try again")
 
     
