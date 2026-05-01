d444434755d103e9b9febaf532011627e7bdbe89
ZHEL <47091459+zhel98@users.noreply.github.com>
2026-05-01 20:48:44 +0600

    123

--- python/GeeksHW0_10/flet_start/main.py
+++ python/GeeksHW0_10/flet_start/main.py
@@ -1,8 +1,15 @@
 import flet as ft
 
 def main_page(page: ft.Page):
-    hello_text = ft.Text(value='Hello World')
+    page.title = 'Мое первое приложение'
+    page.theme_mode = ft.ThemeMode.LIGHT
     
-    page.add(hello_text)
+    hello_text = ft.Text(value='Hello World')
+
+    name_input = ft.TextField(max_length=5, label='ты пидор')
+    
+    page.add(hello_text, name_input)
+
 
 ft.app(main_page)
+# ft.app(main_page, view=ft.AppView.WEB_BROWSER)
