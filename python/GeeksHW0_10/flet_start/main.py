b33a5a652c0a960333327802ae9336b716316584
ZHEL <47091459+zhel98@users.noreply.github.com>
2026-05-05 21:01:30 +0600

    123

--- python/GeeksHW0_10/flet_start/main.py
+++ python/GeeksHW0_10/flet_start/main.py
@@ -6,9 +6,39 @@ def main_page(page: ft.Page):
     
     hello_text = ft.Text(value='Hello World')
 
-    name_input = ft.TextField(max_length=5, label='ты пидор')
     
-    page.add(hello_text, name_input)
+    def on_button_click(_):
+        # print(name_input.value)
+        # print(f'{_}')
+        name = name_input.value.strip()
+        
+        if name:
+            hello_text.color= None
+            hello_text.value= f'Hello {name}'
+            name_input.value = None
+            # print(name)
+        else:
+            # print("Error")
+            hello_text.value = 'Ошибка введите имя'
+            hello_text.color = ft.Colors.RED
+        page.update()
+        
+    def change_them(_):
+        if page.theme_mode == ft.ThemeMode.DARK:
+            page.theme_mode = ft.ThemeMode.LIGHT
+        else:
+            page.theme_mode = ft.ThemeMode.DARK
+        page.update()
+        
+    change_theme_button = ft.IconButton(ft.Icons.BRIGHTNESS_6, on_click=change_them)
+    name_input = ft.TextField(max_length=5, label='ты пидор', on_submit=on_button_click)
+    elevated_button = ft.ElevatedButton(text = 'SEND', icon=ft.Icons.SEND, on_click=on_button_click)
+    # text_button = ft.TextButton(text='SEND', icon=ft.Icons.SEND)
+    # icon_button = ft.IconButton(icon=ft.Icons.SEND) #не прнимает текст в отличии от своих соседей
+    
+    page.add(hello_text, name_input, elevated_button, change_theme_button)
+
+
 
 
 ft.app(main_page)
